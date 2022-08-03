85e7265f06dbc21c2da625158f9fd475d08ba127
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-04 00:34:24 +0300

    Student grade problem solution added

---
+++ student_grade.py
@@ -0,0 +1,12 @@
+def gradingStudents(grades):
+    remainder=0;
+    for i in range(0,len(grades)):
+        remainder=grades[i]%5
+        if remainder>2 and grades[i]>37:
+            grades[i] += 5-remainder
+        
+    return grades
+n = int(input("Enter number of grades : "))   
+grades = list(map(int,input("\nEnter the numbers : ").strip().split()))[:n] 
+output = gradingStudents(grades)
+print(output)
