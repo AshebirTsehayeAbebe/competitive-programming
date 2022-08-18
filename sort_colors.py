6bd335ec44f4b988d7cf7c60d80b4dfa9ea4d3c1
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-19 00:25:52 +0300

    Some modifications

--- sort_colors.py
+++ sort_colors.py
@@ -1,41 +1,44 @@
+from inspect import _void
 from re import L
-
-
-def mergeSort(nums):
- if len(nums)>1:
-  length=len(nums)
-  medium=len(nums)//2
-  L=nums[:medium]
-  M=nums[medium:]
+from typing import List
+class Solution:
+  def mergeSort(self, nums: List[int])->None:
+    if len(nums)>1:
+      length=len(nums)
+      medium=len(nums)//2
+      L=nums[:medium]
+      M=nums[medium:]
+      
+      self.mergeSort(L)
+      self.mergeSort(M)
+      
+      i=j=k=0
+      
+      while(i<len(L) and j<len(M)):
+        if(L[i]<M[j]):
+          nums[k]=L[i]
+          i+=1
+        else:
+          nums[k]=M[j]
+          j+=1
+        k+=1
+      
+      while(i<len(L)):
+        nums[k]=L[i]
+        i+=1
+        k+=1
+        
+      while(j<len(M)):
+        nums[k]=M[j]
+        j+=1
+        k+=1
   
-  mergeSort(L)
-  mergeSort(M)
   
-  i=j=k=0
+  def sortColors(self, nums) -> None:
+    self.mergeSort(nums) 
+    for i in range(len(nums)):  
+      print(nums[i])
   
-  while(i<len(L) and j<len(M)):
-   if(L[i]<M[j]):
-    nums[k]=L[i]
-    i+=1
-   else:
-    nums[k]=M[j]
-    j+=1
-   k+=1
-  
-  while(i<len(L)):
-    nums[k]=L[i]
-    i+=1
-    k+=1
-    
-  while(j<len(M)):
-    nums[k]=M[j]
-    j+=1
-    k+=1
- 
- 
-def sortColors(nums) -> None:
- mergeSort(nums) 
- for i in range(len(nums)):  
-  print(nums[i])
 nums=[2,1,0,2,1,2,0]
-sortColors(nums)
+obj = Solution()
+obj.sortColors(nums)
