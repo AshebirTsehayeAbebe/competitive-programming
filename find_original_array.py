8e6ed8bfe839fcaa55e099d0744876b59629de65
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-19 18:08:13 +0300

    modification in find original array solution

--- find_original_array.py
+++ find_original_array.py
@@ -1,27 +1,24 @@
+from typing import List
 class Solution:
-    def findOriginalArray(self, changed):
-        output = []
-        changed.sort()
-        freNumber = [0] *(changed[len(changed)-1]+1) 
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
         if len(changed)  % 2 !=0 and len(changed)==0:
             return []
+        output = []
+        changed.sort()
+        frequencyNumber = [0] *(2*changed[len(changed)-1]+1) 
         for i in changed:
-            freNumber[i]+=1
+            frequencyNumber[i]+=1
         for i in changed:
-            if freNumber[i]>0:                
-                freNumber[i]-=1
-                if 2*i in changed: 
-                    if freNumber[2*i]>0:
-                       freNumber[2*i]-=1          
-                       output.append(i)
-                    else:
-                        return []
-        if len(output)!=len(changed)/2:
-            return []
+            if frequencyNumber[i]>0:                
+                frequencyNumber[i]-=1
+                if frequencyNumber[2*i]>0:
+                   frequencyNumber[2*i]-=1        
+                   output.append(i)
+                else:
+                    return []
         return output
+    
 obj = Solution()
-size = int(input('Enter size: '))
-doubleArray = list(map(int, input("\nEnter the elements : ").strip().split()))[:size]
-print(doubleArray)
-originalArray = obj.findOriginalArray(doubleArray)
-print(originalArray)
+n = int(input('Enter size: '))
+changed = list(map(int, input("\nEnter the elements : ").strip().split()))[:n]
+print(obj.findOriginalArray(changed))
--- kth_largest_number_previous.py
+++ kth_largest_number_previous.py
@@ -1,19 +1,20 @@
-def partition(elements, low, high):
- pivotElement = elements[high]
- pointer = low-1
+class Solution:
+  def partition(self, elements, low, high):
+    pivotElement = elements[high]
+    pointer = low-1
  
- for i in range(low, high):
-  if elements[i]<pivotElement:
-   pointer+=1
-   elements[pointer], elements[i] = elements[i], elements[pointer]
- elements[pointer+1], elements[high] = elements[high], elements[pointer+1]
- return pointer+1
-def kthLargestNumber(elements, low, high):
- if low< high:
-  pi = partition(elements, low, high)
-  kthLargestNumber(elements, low, pi-1)
-  kthLargestNumber(elements, pi+1, high)
- 
+    for i in range(low, high):
+      if elements[i]<pivotElement:
+        pointer+=1
+        elements[pointer], elements[i] = elements[i], elements[pointer]
+    elements[pointer+1], elements[high] = elements[high], elements[pointer+1]  
+    return pointer+1
+  def kthLargestNumber(self, elements, low, high):
+    if low< high:
+      pi = self.partition(elements, low, high)
+      self.kthLargestNumber(elements, low, pi-1)
+      self.kthLargestNumber(elements, pi+1, high)
+      
 
 n = int(input("Enter number of elements : "))   
 elements = list(map(int,input("\nEnter the elements : ").strip().split()))[:n]
