0b82d30a1f317a02eb2679f0255ef9c5d8857982
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-20 08:40:14 +0300

    Mofication in num_identical_pairs  solution

--- num_identical_pairs.py
+++ num_identical_pairs.py
@@ -1,10 +1,13 @@
-def numIdenticalPairs(nums):
- output=0
- for i, item in enumerate(nums):
-     for j in range(i+1, len(nums)):
-         if item==nums[j]:
-             output+=1
- return output
+from typing import List
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        output=0
+        for i, item in enumerate(nums):
+            for j in range(i+1, len(nums)):
+                if item==nums[j]:
+                    output+=1
+        return output
+obj = Solution()
 size = int(input('Enter size: '))
 nums = list(map(int, input('Enter elements: ').strip().split()[:size]))
-print(numIdenticalPairs(nums))
+print(obj.numIdenticalPairs([1,2,3]))
