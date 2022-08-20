1a67304a14389ecbdfb69a1c25b841c3e13dde20
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-20 08:19:47 +0300

    Mofication in max frequency solution

--- max_frequency.py
+++ max_frequency.py
@@ -1,28 +1,21 @@
-from collections import Counter
-def maxFrequency(nums, k):
- if len(nums)==0:
-  return 0
- numCounter = Counter(nums)
- sortedNums = sorted(numCounter.items(),key = lambda i: i[0])
- if len(sortedNums)==1:
-  return sortedNums[0][1] 
- answer=1 
- for i in range(1, len(sortedNums)):
-  count = 0
-  tracker=i-1
-  element = sortedNums[tracker][0]
-  temAns = sortedNums[i][1]
-  print(temAns)
-  while count <= k and tracker>=0:
-   if element==sortedNums[i][0]:
-     temAns+=sortedNums[tracker][1]
-     tracker-=1
-     element = sortedNums[tracker][0]
-     continue
-   element+=1   
-   count+=1
- answer=max(answer, temAns)
- return answer
-elements  = list(map(int,input('Enter elements: ').strip().split()[:5]))
-print(maxFrequency(elements, 5))
+from typing import List
+class Solution:
+   def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        maxFrequency, sum, start, end=0,0,0,0
+
+        for end in range(len(nums)):
+            sum+=nums[end]        
+            while (end -start+1)*nums[end]-sum>k:
+                sum-=nums[start]
+                start+=1
+            maxFrequency=max(maxFrequency, end -start+1)
+        return maxFrequency
+
+obj = Solution()
+n=int(input('Enter array size: '))
+nums  = list(map(int,input('Enter elements: ').strip().split()[:5]))
+
+k=int(input('Enter number of operations: '))
+print(obj.maxFrequency(nums, k))
     
