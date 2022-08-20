3e8e2cd8eb380fcff94dc511e6f8ae042f335577
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-20 18:12:28 +0300

    Modification in number_of_squares

--- number_of_squares.py
+++ number_of_squares.py
@@ -1,12 +1,11 @@
-import math
-def numberOfSquares(m, n, a):
-    
-    widthCount=m//a
-    columnCount = n//a
-    
-    if m%a != 0:
-        widthCount+=1
-    if n%a != 0: 
-        columnCount+=1
-    return widthCount*columnCount
-print(numberOfSquares(6, 6, 4))
+class Solution:
+    def numberOfSquares(self,m: int, n: int, a: int)->int:
+        
+        widthCount=m//a
+        columnCount = n//a
+        
+        if m%a != 0:
+            widthCount+=1
+        if n%a != 0: 
+            columnCount+=1
+        return widthCount*columnCount
