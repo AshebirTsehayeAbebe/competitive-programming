73d9d82c07a6008b5d92a2172d69dda91916df83
Ashebir Tsehaye <ashebir.t@bostonadvancedlab.com>
2022-08-20 08:52:00 +0300

    Mofication in counting_valleys  solution

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,20 +1,21 @@
 import string
 
+class Solution:
+    def countingValleys(self, steps, path):
+        up, down, numberOfValleys, previousDown=0,0,0,0
+        for element in path:        
+            previousDown=down
+            if element=='U':
+                up+=1
+                down-=1
+            if element=='D':
+                up-=1
+                down+=1
+            if previousDown>0 and down==up:
+                numberOfValleys+=1
+        return numberOfValleys
 
-def countingValleys(steps, path):
-    up, down, numberOfValleys, previousDown=0,0,0,0
-    for element in path:        
-        previousDown=down
-        if element=='U':
-            up+=1
-            down-=1
-        if element=='D':
-            up-=1
-            down+=1
-        if previousDown>0 and down==up:
-            numberOfValleys+=1
-    return numberOfValleys
+obj = Solution()
 size = int(input('Enter size: '))
 elements = list(input('Enter elements: '))
-print(elements)
-print(countingValleys(size, elements))
+print(obj.countingValleys(size, elements))
