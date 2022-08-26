74cdc0a60a3c79bc733a002c5db06d889ff04cec
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-08-26 21:44:59 +0300

    Update merge_overlapping_intervals.py

--- merge_overlapping_intervals.py
+++ merge_overlapping_intervals.py
@@ -1,24 +1,20 @@
-def merge(intervals):
-        output = []
-        for i in range(1, len(intervals)):
-            j=i-1
-            key = intervals[i]
-            
-            while(j>=0 and key[0]<intervals[j][0]):
-                intervals[j+1]=intervals[j]
-                j-=1
-            intervals[j+1]=key
-        
-        tempInterval=intervals[0]
-        for currentInterval in intervals:
-            if currentInterval[0]<=tempInterval[1]:
-                tempInterval[1]=max(tempInterval[1], currentInterval[1])
-            else:
-                output.append(tempInterval)
-                tempInterval=currentInterval
-        print(intervals)     
-        output.append(tempInterval)
-        return output
+class Solution:    
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        result = []
+        intervals.sort(key = lambda l:l[0])
+        print(intervals)
+        for i in range(len(intervals)):
+            if i == 0:
+                result.append(intervals[i])
+                continue
+
+            lastInterval = result[len(result) - 1]
+            if lastInterval[1] < intervals[i][0]:
+                result.append(intervals[i])
+            elif lastInterval[1] >= intervals[i][0]:
+                lastInterval[1] = max(intervals[i][1],lastInterval[1])
+
+        return result;
         
 input = [[1,3],[2,6],[8,10],[15,18]]   
 output = merge(input)
