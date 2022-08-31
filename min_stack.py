e5a2dc302ae76ea64777f15b6914dda3bdba85b6
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-08-31 22:37:14 +0300

    Create min_stack.py

---
+++ min_stack.py
@@ -0,0 +1,24 @@
+class MinStack:
+    def __init__(self):
+        self.p = []
+        self.m = 9999999999999
+    def push(self, x: int) -> None:
+        self.p.append(x)
+        self.m=min(self.m,x)
+        
+    def pop(self) -> None:
+        tem=self.p.pop()
+        if tem==self.m:
+            if self.p:
+                self.m=min(self.p)
+            else:
+                self.m=9999999999999
+                
+    def top(self) -> int:
+        return self.p[-1]
+
+    def getMin(self) -> int:
+        if self.p:
+            return self.m
+        else:
+            return None
