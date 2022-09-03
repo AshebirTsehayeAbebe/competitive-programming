7914590e3d056d319e8898980859f1a26f00a75e
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-09-03 11:03:15 +0300

    Create move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i= count = 0
+        while count < len(nums):
+            if nums[i] == 0: nums.append(nums.pop(i))
+            else: i += 1
+            count += 1
+                    
+        return nums
