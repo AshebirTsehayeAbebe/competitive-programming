99bc12ba71a1d4384138f861b60d4bd101bcf66a
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-09-28 08:49:11 +0300

    Update add_two_numbers.py

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,7 +1,7 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode(0)
-        curr = dummyHead
+        newList = ListNode(0)
+        curr = newList
         carry = 0
         while l1 != None or l2 != None or carry != 0:
             l1Val = l1.val if l1 else 0
@@ -13,4 +13,4 @@ class Solution:
             curr = newNode
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return dummyHead.next
+        return newList.next
