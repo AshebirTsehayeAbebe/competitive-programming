40e31ec2abe592eda6cb27284ea7e11de57dfb3c
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-09-29 11:24:24 +0300

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,9 +1,21 @@
+# Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         def checkPalindrome(left, right):
+#             if right is None:
+#                 return True, left
+
+#             val, left = checkPalindrome(left, right.next)
+
+#             result = val and (left.val == right.val)
+#             left = left.next
+
+#             return result, left
+#         return checkPalindrome(head, head)[0]
             
         prev, curr =None, head
         list=[]
