b7449c1fe6d93dfcf21748ee5790f1d737f2f2ec
AshebirTsehayeAbebe <51706783+AshebirTsehayeAbebe@users.noreply.github.com>
2022-10-01 01:08:43 +0300

    Create min_increment_for_unique.py

---
+++ min_increment_for_unique.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        max_val = max(nums)
+        count = collections.Counter(nums)
+        taken = []
+        
+        moves = 0
+        print(count)
+        for x in range(len(nums) + max_val):
+            if count[x] >= 2:
+                taken.extend([x] * (count[x] - 1))
+            elif taken and count[x] == 0:
+                moves += x - taken.pop()
+            print(taken)
+                
+        return moves
